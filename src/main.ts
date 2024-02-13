f3db133370f95ac820825028ef5748727fbef433
nattkpk <118959982+nattkpk@users.noreply.github.com>
2024-02-13 14:09:54 +0700

    deploy1

--- src/main.ts
+++ src/main.ts
@@ -5,11 +5,44 @@ import { bootstrapExtra } from "@workadventure/scripting-api-extra";
 console.log('Script started successfully');
 
 let currentPopup: any = undefined;
+let check = false;
 
 // Waiting for the API to be ready
 WA.onInit().then(() => {
     console.log('Scripting API ready');
     console.log('Player tags: ',WA.player.tags)
+
+    WA.ui.actionBar.addButton({
+        id: "btn-trueeye",
+        type: "action",
+        imageSrc: "https://i.ibb.co/8452NqN/rocket-lunchx.png",
+        toolTip: "AI ThaiGen",
+        callback: () => {
+          if (check) {
+            WA.ui.modal.closeModal();
+            check = false;
+            return;
+          }
+          aithaigen();
+          check = true;
+        },
+      });
+  
+      WA.ui.actionBar.addButton({
+        id: "btn1",
+        type: "action",
+        imageSrc: "https://i.ibb.co/JFXcrqH/computerx.png",
+        toolTip: "Coding Thailand",
+        callback: () => {
+          if (check) {
+            WA.ui.modal.closeModal();
+            check = false;
+            return;
+          }
+          codingthailand();
+          check = true;
+        },
+      });
 
     WA.room.area.onEnter('clock').subscribe(() => {
         const today = new Date();
@@ -32,5 +65,29 @@ function closePopup(){
         currentPopup = undefined;
     }
 }
+
+const codingthailand = async () => {
+    WA.ui.modal.closeModal();
+    WA.ui.modal.openModal({
+        src: "https://codingthailand.org/",
+        allow: "fullscreen",
+        title: "website",
+        allowApi: true,
+        position: "center",
+    });
+  
+  };
+  
+  const aithaigen = async () => {
+    WA.ui.modal.closeModal();
+    WA.ui.modal.openModal({
+      src: "https://aithaigen.in.th/home",
+      allow: "fullscreen",
+      title: "website",
+      allowApi: true,
+      position: "center",
+    });
+  };
+  
 
 export {};
