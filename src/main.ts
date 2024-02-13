f957ad6ac7132381d882a038049817c50133b5b2
nattkpk <118959982+nattkpk@users.noreply.github.com>
2024-02-13 14:14:46 +0700

    hi

--- src/main.ts
+++ src/main.ts
@@ -2,92 +2,93 @@
 
 import { bootstrapExtra } from "@workadventure/scripting-api-extra";
 
-console.log('Script started successfully');
+console.log("Script started successfully");
 
 let currentPopup: any = undefined;
 let check = false;
 
 // Waiting for the API to be ready
-WA.onInit().then(() => {
-    console.log('Scripting API ready');
-    console.log('Player tags: ',WA.player.tags)
+WA.onInit()
+  .then(() => {
+    console.log("Scripting API ready");
+    console.log("Player tags: ", WA.player.tags);
 
     WA.ui.actionBar.addButton({
-        id: "btn-trueeye",
-        type: "action",
-        imageSrc: "https://i.ibb.co/8452NqN/rocket-lunchx.png",
-        toolTip: "AI ThaiGen",
-        callback: () => {
-          if (check) {
-            WA.ui.modal.closeModal();
-            check = false;
-            return;
-          }
-          aithaigen();
-          check = true;
-        },
-      });
-  
-      WA.ui.actionBar.addButton({
-        id: "btn1",
-        type: "action",
-        imageSrc: "https://i.ibb.co/JFXcrqH/computerx.png",
-        toolTip: "Coding Thailand",
-        callback: () => {
-          if (check) {
-            WA.ui.modal.closeModal();
-            check = false;
-            return;
-          }
-          codingthailand();
-          check = true;
-        },
-      });
+      id: "btn-trueeye",
+      type: "action",
+      imageSrc: "https://i.ibb.co/8452NqN/rocket-lunchx.png",
+      toolTip: "AI ThaiGen",
+      callback: () => {
+        if (check) {
+          WA.ui.modal.closeModal();
+          check = false;
+          return;
+        }
+        aithaigen();
+        check = true;
+      },
+    });
 
-    WA.room.area.onEnter('clock').subscribe(() => {
-        const today = new Date();
-        const time = today.getHours() + ":" + today.getMinutes();
-        currentPopup = WA.ui.openPopup("clockPopup", "It's " + time, []);
-    })
+    WA.ui.actionBar.addButton({
+      id: "btn1",
+      type: "action",
+      imageSrc: "https://i.ibb.co/JFXcrqH/computerx.png",
+      toolTip: "Coding Thailand",
+      callback: () => {
+        if (check) {
+          WA.ui.modal.closeModal();
+          check = false;
+          return;
+        }
+        codingthailand();
+        check = true;
+      },
+    });
 
-    WA.room.area.onLeave('clock').subscribe(closePopup)
+    WA.room.area.onEnter("clock").subscribe(() => {
+      const today = new Date();
+      const time = today.getHours() + ":" + today.getMinutes();
+      currentPopup = WA.ui.openPopup("clockPopup", "It's " + time, []);
+    });
+
+    WA.room.area.onLeave("clock").subscribe(closePopup);
 
     // The line below bootstraps the Scripting API Extra library that adds a number of advanced properties/features to WorkAdventure
-    bootstrapExtra().then(() => {
-        console.log('Scripting API Extra ready');
-    }).catch(e => console.error(e));
+    bootstrapExtra()
+      .then(() => {
+        console.log("Scripting API Extra ready");
+      })
+      .catch((e) => console.error(e));
+  })
+  .catch((e) => console.error(e));
 
-}).catch(e => console.error(e));
-
-function closePopup(){
-    if (currentPopup !== undefined) {
-        currentPopup.close();
-        currentPopup = undefined;
-    }
+function closePopup() {
+  if (currentPopup !== undefined) {
+    currentPopup.close();
+    currentPopup = undefined;
+  }
 }
 
 const codingthailand = async () => {
-    WA.ui.modal.closeModal();
-    WA.ui.modal.openModal({
-        src: "https://codingthailand.org/",
-        allow: "fullscreen",
-        title: "website",
-        allowApi: true,
-        position: "center",
-    });
-  
-  };
-  
-  const aithaigen = async () => {
-    WA.ui.modal.closeModal();
-    WA.ui.modal.openModal({
-      src: "https://aithaigen.in.th/home",
-      allow: "fullscreen",
-      title: "website",
-      allowApi: true,
-      position: "center",
-    });
-  };
-  
+  WA.ui.modal.closeModal();
+  WA.ui.modal.openModal({
+    src: "https://codingthailand.org/",
+    allow: "fullscreen",
+    title: "website",
+    allowApi: true,
+    position: "center",
+  });
+};
+
+const aithaigen = async () => {
+  WA.ui.modal.closeModal();
+  WA.ui.modal.openModal({
+    src: "https://aithaigen.in.th/home",
+    allow: "fullscreen",
+    title: "website",
+    allowApi: true,
+    position: "center",
+  });
+};
 
 export {};
