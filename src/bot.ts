f2873b46679fbee96fbc81d4ffdb2eb0e8ec065e
nattkpk <118959982+nattkpk@users.noreply.github.com>
2024-03-21 11:07:25 +0700

    fixBotScript

--- src/bot.ts
+++ src/bot.ts
@@ -13,16 +13,29 @@ WA.room.area.onEnter("bot_spawn").subscribe(() => {
 });
 export {};
 
-let x = 4655;
-let y = 2094;
+let x = 4600;
+let y = 1900;
 function botrun() {
   console.log("Start Walking");
-  setTimeout(function () {
-    const deltaX = Math.floor(Math.random() * 300) - 150;
-    const deltaY = Math.floor(Math.random() * 300) - 150;
+  
+    const deltaX = Math.floor(Math.random() * 700) - 350;
+    const deltaY = Math.floor(Math.random() * 700) - 350;
     x += deltaX;
     y += deltaY;
+    if (y > 2440) {
+      y = 2420;
+    }
+    if (x > 4840) {
+      x = 4780;
+    }
     WA.player.moveTo(x, y, 10);
+    setTimeout(function () {
+      WA.onInit().then(async () => {
+        console.log('St Position : ', await WA.player.getPosition());
+        // WA.player.onPlayerMove(console.log);
+    })
+    console.log('to Position X: ',x); 
+    console.log('to Position Y: ',y);
     botrun();
-  }, Math.floor(Math.random() * 3000) + 2000);
+  }, Math.floor(Math.random() * 4000) + 6000);
 }
