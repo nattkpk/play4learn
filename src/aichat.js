9b26a992397020ad7d07193cd184ee5ca25d8f2b
nattkpk <118959982+nattkpk@users.noreply.github.com>
2024-03-22 11:57:08 +0700

    fix

--- src/aichat.js
+++ src/aichat.js
@@ -7,7 +7,7 @@ const genAI = new GoogleGenerativeAI("AIzaSyCjSWKop9h4C9iZHHaTVwhRA340Awbj0gY");
 async function run(prompt) {
   const model = genAI.getGenerativeModel({ model: "gemini-pro" });
 
-  const result = await model.generateContent(prompt + " ให้เข้าใจง่าย");
+  const result = await model.generateContent(prompt + " ให้เหลือสั้นๆเข้าใจง่าย");
   const response = await result.response;
   const text = response.text();
   return text;
